2105915c6f5d638bf091ae6ed2f6ae12ed96ea33
ANH KHOA <nakhoa232@clc.fitus.edu.vn>
2024-03-30 09:03:32 +0700

    Update library

---
+++ lib.h
@@ -0,0 +1,22 @@
+#pragma once
+#define _WIN32_WINNT 0x0500
+
+//Input and output format library
+#include <iostream>
+#include <iomanip>
+
+// Game logic and algorithm library
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <deque>
+
+// For console settings
+#include <windows.h>
+
+struct Board{
+    int row = 4; //default row
+    int col = 6; // default column
+    int** board; 
+    char** poke; //display chararter on each cell
+};
